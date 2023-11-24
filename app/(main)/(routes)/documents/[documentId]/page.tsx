4f1b91177d25dc428abd18dacc645c6d4c4ae54a
Kunaaal13 <mrkunalyadav7@gmail.com>
2023-11-24 15:38:36 +0530

    Added emoji and heading

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,7 +1,37 @@
-type DocumentIDPageProps = {}
+'use client'
 
-function DocumentIDPage({}: DocumentIDPageProps) {
-  return <div>Document ID</div>
+import Toolbar from '@/components/toolbar'
+import { api } from '@/convex/_generated/api'
+import { Id } from '@/convex/_generated/dataModel'
+import { useQuery } from 'convex/react'
+
+type DocumentIDPageProps = {
+  params: {
+    documentId: Id<'documents'>
+  }
+}
+
+function DocumentIDPage({ params: { documentId } }: DocumentIDPageProps) {
+  const document = useQuery(api.documents.getById, {
+    documentId,
+  })
+
+  if (document === undefined) {
+    return <div>Loading...</div>
+  }
+
+  if (document === null) {
+    return <div>Not found</div>
+  }
+
+  return (
+    <div className='pb-40'>
+      <div className='h-[35vh]' />
+      <div className='md:max-w-3xl lg:max-w-4xl mx-auto'>
+        <Toolbar initialData={document} />
+      </div>
+    </div>
+  )
 }
 
 export default DocumentIDPage
