45bba426f0b6020e263a59eb92ead7df2c9c52d5
Kunaaal13 <mrkunalyadav7@gmail.com>
2023-11-24 13:15:42 +0530

    Added banner

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -6,6 +6,8 @@ import { useQuery } from 'convex/react'
 import { MenuIcon } from 'lucide-react'
 import { useParams } from 'next/navigation'
 import Title from './title'
+import Banner from './banner'
+import Menu from './menu'
 
 type NavbarProps = {
   isCollapsed: boolean
@@ -20,8 +22,12 @@ function Navbar({ isCollapsed, onResetWidth }: NavbarProps) {
 
   if (document === undefined) {
     return (
-      <nav className='bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center '>
+      <nav className='bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center justify-between'>
         <Title.Skeleton />
+
+        <div className='flex items-center gap-x-2'>
+          <Menu.Skeleton />
+        </div>
       </nav>
     )
   }
@@ -30,19 +36,27 @@ function Navbar({ isCollapsed, onResetWidth }: NavbarProps) {
     return null
   }
   return (
-    <nav className='bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center gap-x-4'>
-      {isCollapsed && (
-        <MenuIcon
-          role='button'
-          onClick={onResetWidth}
-          className='h-6 w-6 text-muted-foreground'
-        />
-      )}
+    <>
+      <nav className='bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center gap-x-4'>
+        {isCollapsed && (
+          <MenuIcon
+            role='button'
+            onClick={onResetWidth}
+            className='h-6 w-6 text-muted-foreground'
+          />
+        )}
 
-      <div className='flex items-center justify-between w-full'>
-        <Title initialData={document} />
-      </div>
-    </nav>
+        <div className='flex items-center justify-between w-full'>
+          <Title initialData={document} />
+
+          <div className='flex items-center gap-x-2'>
+            <Menu documentId={document._id} />
+          </div>
+        </div>
+      </nav>
+
+      {document.isArchived && <Banner documentId={document._id} />}
+    </>
   )
 }
 
